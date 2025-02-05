9a0145795e9a53b9581e3f0b6c10cf9707939eb7
EnergyPatrikHU <energypatrikhu@proton.me>
2025-02-05 20:36:13 +0100

    Enhance d.up command autocomplete functionality

--- commands/docker/start.sh
+++ commands/docker/start.sh
@@ -6,5 +6,18 @@ d.up() {
   fi
 }
 
-. $EPX_PATH/commands/docker/_autocomplete.sh
+# . $EPX_PATH/commands/docker/_autocomplete.sh
+
+_d.autocomplete() {
+  local cur prev opts
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  prev="${COMP_WORDS[COMP_CWORD - 1]}"
+  opts="all $(docker container ls -a --format '{{.Names}}')"
+
+  if [[ ${cur} == -* ]]; then
+    COMPREPLY=($(compgen -W "${opts}" -- ${cur}))
+    return 0
+  fi
+}
 complete -F _d.autocomplete d.up
