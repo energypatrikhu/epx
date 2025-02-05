2f4b0d895364a4143c68fe39a199f130c619de49
EnergyPatrikHU <energypatrikhu@proton.me>
2025-02-05 22:18:30 +0100

    Add d.stop command to stop Docker containers with usage instructions

---
+++ commands/docker/stop.sh
@@ -0,0 +1,25 @@
+d.stop() {
+  if [[ -z $1 ]]; then
+    printf "[${EPX_COLORS["LIGHT_BLUE"]}Docker${EPX_COLORS["NC"]}] ${EPX_COLORS["LIGHT_YELLOW"]}Usage: d.stop <all / container>${EPX_COLORS["NC"]}\n"
+    return
+  fi
+
+  if [[ $1 == "all" ]]; then
+    docker container stop $(docker container ls -a -q) >/dev/null 2>&1
+
+    printf "[${EPX_COLORS["LIGHT_BLUE"]}Docker${EPX_COLORS["NC"]}] All containers ${EPX_COLORS["LIGHT_RED"]}stopped${EPX_COLORS["NC"]}\n"
+  else
+    docker container stop "$@" >/dev/null 2>&1
+
+    if [ $# -eq 1 ]; then
+      container_text="Container"
+    else
+      container_text="Containers"
+    fi
+    containers=$(printf "${EPX_COLORS["LIGHT_BLUE"]}%s${EPX_COLORS["NC"]}, " "$@" | sed 's/, $//')
+    printf "[${EPX_COLORS["LIGHT_BLUE"]}Docker${EPX_COLORS["NC"]}] ${container_text} %s ${EPX_COLORS["LIGHT_RED"]}stopped${EPX_COLORS["NC"]}\n" "$containers"
+  fi
+}
+
+. $EPX_PATH/commands/docker/_autocomplete.sh
+complete -F _d_autocomplete d.stop
