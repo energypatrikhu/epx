93749d07b3a0180357c56b5896036c2445c5c6bd
EnergyPatrikHU <energypatrikhu@proton.me>
2025-02-05 22:42:13 +0100

    Enhance d.list command with state filtering and autocomplete support

--- commands/docker/list.sh
+++ commands/docker/list.sh
@@ -1,6 +1,29 @@
 d.list() {
+  # Default state filter (show all containers)
+  state_filter="all"
+
+  # Parse arguments
+  # if [ "$1" = "running" ]; then
+  #   state_filter="running"
+  # elif [ "$1" = "exited" ]; then
+  #   state_filter="exited"
+  # elif [ "$1" = "all" ]; then
+  #   state_filter="all"
+  # fi
+
+  state_filter=$1
+
   # Fetch container data dynamically using docker command
-  data=$(docker container ls -a --format "{{.ID}}\t{{.Names}}\t{{.Image}}\t{{.Status}}")
+  if [ -z "$state_filter" ]; then
+    data=$(docker container ls -a --format "{{.ID}}\t{{.Names}}\t{{.Image}}\t{{.Status}}")
+  else
+    data=$(docker container ls -a --format "{{.ID}}\t{{.Names}}\t{{.Image}}\t{{.Status}}" --filter "status=$state_filter")
+  fi
+
+  if [ -z "$data" ]; then
+    printf "[${EPX_COLORS["LIGHT_BLUE"]}Docker${EPX_COLORS["NC"]}] ${EPX_COLORS["LIGHT_YELLOW"]}No containers found${EPX_COLORS["NC"]}\n"
+    return
+  fi
 
   # Initialize column widths
   id_width=12    # Minimum width for CONTAINER ID
@@ -56,3 +79,6 @@ EOF
   # Print the table footer
   echo "$separator"
 }
+
+. $EPX_PATH/commands/docker/_autocomplete.sh
+complete -F _d_autocomplete_list d.list
