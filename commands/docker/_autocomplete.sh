9eaaac5590665e44eac9ae41d79511662a43d003
EnergyPatrikHU <energypatrikhu@proton.me>
2025-02-05 22:23:59 +0100

    Add autocomplete support for all Docker containers in _autocomplete.sh

--- commands/docker/_autocomplete.sh
+++ commands/docker/_autocomplete.sh
@@ -1,9 +1,21 @@
+_d_autocomplete_all() {
+  local cur opts
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+
+  # Get the list of all containers and the "all" keyword
+  opts="all $(docker container ls -a --format '{{.Names}}')"
+
+  # Generate autocomplete suggestions
+  COMPREPLY=($(compgen -W "${opts}" -- "$cur"))
+}
+
 _d_autocomplete() {
   local cur opts
   COMPREPLY=()
   cur="${COMP_WORDS[COMP_CWORD]}"
 
-  # Get the list of all stopped containers and "all" option
+  # Get the list of all containers and the "all" keyword
   opts="all $(docker container ls -a --format '{{.Names}}')"
 
   # Generate autocomplete suggestions
