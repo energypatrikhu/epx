649997039a0e7e683bc0dcc1bfdc9514cad2b5bb
EnergyPatrikHU <energypatrikhu@proton.me>
2025-02-05 22:26:07 +0100

    Add d.shell command with usage instructions and autocomplete support

---
+++ commands/docker/shell.sh
@@ -0,0 +1,11 @@
+d.shell() {
+  if [[ -z $1 ]]; then
+    printf "[${EPX_COLORS["LIGHT_BLUE"]}Docker${EPX_COLORS["NC"]}] ${EPX_COLORS["LIGHT_YELLOW"]}Usage: d.shell <container>${EPX_COLORS["NC"]}\n"
+    return
+  fi
+
+  docker exec -it "$1" /bin/bash
+}
+
+. $EPX_PATH/commands/docker/_autocomplete.sh
+complete -F _d_autocomplete d.shell
