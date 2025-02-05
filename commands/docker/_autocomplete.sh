b55001aacdf28fe83dbe64e55ff69b92110c61e3
EnergyPatrikHU <energypatrikhu@proton.me>
2025-02-05 22:18:11 +0100

    Refactor autocomplete

--- commands/docker/_autocomplete.sh
+++ commands/docker/_autocomplete.sh
@@ -1,12 +1,11 @@
-_d.autocomplete() {
-  local cur prev opts
+_d_autocomplete() {
+  local cur opts
   COMPREPLY=()
   cur="${COMP_WORDS[COMP_CWORD]}"
-  prev="${COMP_WORDS[COMP_CWORD - 1]}"
+
+  # Get the list of all stopped containers and "all" option
   opts="all $(docker container ls -a --format '{{.Names}}')"
 
-  if [[ ${cur} == -* ]]; then
-    COMPREPLY=($(compgen -W "${opts}" -- ${cur}))
-    return 0
-  fi
+  # Generate autocomplete suggestions
+  COMPREPLY=($(compgen -W "${opts}" -- "$cur"))
 }
