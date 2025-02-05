e71e83c1116e07686b25114a6c65e28456421d7e
EnergyPatrikHU <energypatrikhu@proton.me>
2025-02-05 19:24:59 +0100

    Refactor self-update script to improve directory validation and streamline git pull process

--- utils/self-update.sh
+++ utils/self-update.sh
@@ -1,19 +1,17 @@
-#!/bin/bash
-
-__self_update() {
-  EPX_PATH=$1
-
-  #   if [ ! -d $EPX_PATH ]; then
-  #     echo "
-  # > The '$EPX_PATH' directory does not exist"
-  #     return
-  #   fi
+__epx_self_update() {
+  if [ ! -d $EPX_PATH ]; then
+    echo "
+  > The '$EPX_PATH' directory does not exist"
+    return
+  fi
 
   echo $EPX_PATH
 
-  # cd $EPX_PATH
+  cd $EPX_PATH
 
-  # git pull
+  git pull
 
-  # source ~/.bashrc
+  source ~/.bashrc
+
+  cd -
 }
