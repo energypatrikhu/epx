725bbd25985686c7a333a894c7a32c2867a888e7
EnergyPatrikHU <energypatrikhu@proton.me>
2025-02-05 22:18:40 +0100

    Add d.restart command with usage instructions and autocomplete support

---
+++ commands/docker/restart.sh
@@ -0,0 +1,25 @@
+d.restart() {
+  if [[ -z $1 ]]; then
+    printf "[${EPX_COLORS["LIGHT_BLUE"]}Docker${EPX_COLORS["NC"]}] ${EPX_COLORS["LIGHT_YELLOW"]}Usage: d.restart <all / container>${EPX_COLORS["NC"]}\n"
+    return
+  fi
+
+  if [[ $1 == "all" ]]; then
+    docker container restart $(docker container ls -a -q) >/dev/null 2>&1
+
+    printf "[${EPX_COLORS["LIGHT_BLUE"]}Docker${EPX_COLORS["NC"]}] All containers ${EPX_COLORS["LIGHT_CYAN"]}restarted${EPX_COLORS["NC"]}\n"
+  else
+    docker container restart "$@" >/dev/null 2>&1
+
+    if [ $# -eq 1 ]; then
+      container_text="Container"
+    else
+      container_text="Containers"
+    fi
+    containers=$(printf "${EPX_COLORS["LIGHT_BLUE"]}%s${EPX_COLORS["NC"]}, " "$@" | sed 's/, $//')
+    printf "[${EPX_COLORS["LIGHT_BLUE"]}Docker${EPX_COLORS["NC"]}] ${container_text} %s ${EPX_COLORS["LIGHT_CYAN"]}restarted${EPX_COLORS["NC"]}\n" "$containers"
+  fi
+}
+
+. $EPX_PATH/commands/docker/_autocomplete.sh
+complete -F _d_autocomplete d.restart
