860f82b336a5d1497bf0bdb83810e4d34be743b0
EnergyPatrikHU <energypatrikhu@proton.me>
2025-02-05 22:24:55 +0100

    Remove "all" keyword from autocomplete options for Docker containers

--- commands/docker/_autocomplete.sh
+++ commands/docker/_autocomplete.sh
@@ -16,7 +16,7 @@ _d_autocomplete() {
   cur="${COMP_WORDS[COMP_CWORD]}"
 
   # Get the list of all containers and the "all" keyword
-  opts="all $(docker container ls -a --format '{{.Names}}')"
+  opts="$(docker container ls -a --format '{{.Names}}')"
 
   # Generate autocomplete suggestions
   COMPREPLY=($(compgen -W "${opts}" -- "$cur"))
