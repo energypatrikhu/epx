293fe50d17d0498aeae62fad6fa2cfa48d3c09f3
EnergyPatrikHU <energypatrikhu@proton.me>
2025-02-05 22:18:36 +0100

    Add d.start command with usage instructions and improve output formatting

--- commands/docker/start.sh
+++ commands/docker/start.sh
@@ -1,23 +1,25 @@
-d.up() {
+d.start() {
+  if [[ -z $1 ]]; then
+    printf "[${EPX_COLORS["LIGHT_BLUE"]}Docker${EPX_COLORS["NC"]}] ${EPX_COLORS["LIGHT_YELLOW"]}Usage: d.start <all / container>${EPX_COLORS["NC"]}\n"
+    return
+  fi
+
   if [[ $1 == "all" ]]; then
-    docker container start $(docker container ls -a -q)
+    docker container start $(docker container ls -a -q) >/dev/null 2>&1
+
+    printf "[${EPX_COLORS["LIGHT_BLUE"]}Docker${EPX_COLORS["NC"]}] All containers ${EPX_COLORS["LIGHT_GREEN"]}started${EPX_COLORS["NC"]}\n"
   else
-    docker container start $1
+    docker container start "$@" >/dev/null 2>&1
+
+    if [ $# -eq 1 ]; then
+      container_text="Container"
+    else
+      container_text="Containers"
+    fi
+    containers=$(printf "${EPX_COLORS["LIGHT_BLUE"]}%s${EPX_COLORS["NC"]}, " "$@" | sed 's/, $//')
+    printf "[${EPX_COLORS["LIGHT_BLUE"]}Docker${EPX_COLORS["NC"]}] ${container_text} %s ${EPX_COLORS["LIGHT_GREEN"]}started${EPX_COLORS["NC"]}\n" "$containers"
   fi
 }
 
-# . $EPX_PATH/commands/docker/_autocomplete.sh
-
-_d.autocomplete() {
-  local cur prev opts
-  COMPREPLY=()
-  cur="${COMP_WORDS[COMP_CWORD]}"
-  prev="${COMP_WORDS[COMP_CWORD - 1]}"
-  opts="all $(docker container ls -a --format '{{.Names}}')"
-
-  if [[ ${cur} == -* ]]; then
-    COMPREPLY=($(compgen -W "${opts}" -- ${cur}))
-    return 0
-  fi
-}
-complete -F _d.autocomplete d.up
+. $EPX_PATH/commands/docker/_autocomplete.sh
+complete -F _d_autocomplete d.start
