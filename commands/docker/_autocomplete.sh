a8ee014030014881de9e98fcc180dd1ba4a81596
EnergyPatrikHU <energypatrikhu@proton.me>
2025-02-06 11:24:10 +0100

    Refactor autocomplete functions to reduce redundancy and improve clarity

--- commands/docker/_autocomplete.sh
+++ commands/docker/_autocomplete.sh
@@ -1,29 +1,20 @@
-_d_autocomplete_all() {
+_d_autocomplete_base() {
   local cur opts
   COMPREPLY=()
   cur="${COMP_WORDS[COMP_CWORD]}"
-
-  opts="all $(docker container ls -a --format '{{.Names}}')"
+  opts=$@
 
   COMPREPLY=($(compgen -W "${opts}" -- "$cur"))
 }
 
 _d_autocomplete() {
-  local cur opts
-  COMPREPLY=()
-  cur="${COMP_WORDS[COMP_CWORD]}"
+  _d_autocomplete_base "$(docker container ls --format '{{.Names}}')"
+}
 
-  opts="$(docker container ls -a --format '{{.Names}}')"
-
-  COMPREPLY=($(compgen -W "${opts}" -- "$cur"))
+_d_autocomplete_all() {
+  _d_autocomplete_base "all $(docker container ls -a --format '{{.Names}}')"
 }
 
 _d_autocomplete_list() {
-  local cur opts
-  COMPREPLY=()
-  cur="${COMP_WORDS[COMP_CWORD]}"
-
-  opts="created restarting running removing paused exited dead"
-
-  COMPREPLY=($(compgen -W "${opts}" -- "$cur"))
+  _d_autocomplete_base "created restarting running removing paused exited dead"
 }
