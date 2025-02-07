3e9c8fdc5de743d0d9bdd7aaf15a6a1bbaa30739
EnergyPatrikHU <energypatrikhu@proton.me>
2025-02-07 22:33:40 +0100

    Enhance py.venv.sh to create and activate virtual environment if it doesn't exist

--- commands/python/py.venv.sh
+++ commands/python/py.venv.sh
@@ -14,9 +14,20 @@ py.venv() {
     return 1
   fi
 
-  # check if virtual environment exists
+  # check if virtual environment exists, if not create it, then activate
   if [ ! -d .venv ]; then
-    printf "%s\n" "[$(_c LIGHT_BLUE "Python - VENV")] $(_c LIGHT_RED "Virtual environment does not exist")"
+    printf "%s\n" "[$(_c LIGHT_BLUE "Python - VENV")] Creating virtual environment"
+    python3 -m venv .venv
+
+    # check if virtual environment is created
+    if [ ! -d .venv ]; then
+      printf "%s\n" "[$(_c LIGHT_BLUE "Python - VENV")] $(_c LIGHT_RED "Failed to create virtual environment")"
+      return 1
+    fi
+
+    # activate virtual environment
+    printf "%s\n" "[$(_c LIGHT_BLUE "Python - VENV")] Activating virtual environment"
+    source .venv/bin/activate
     return 1
   fi
 
