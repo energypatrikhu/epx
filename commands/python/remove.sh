d31ec1474f5e7d5537bd08a6ab537637ba5f9629
EnergyPatrikHU <energypatrikhu@proton.me>
2025-02-07 22:59:01 +0100

    Add py.remove script for uninstalling Python packages

---
+++ commands/python/remove.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+py.remove() {
+  # check if Python is installed
+  if ! command -v python3 &>/dev/null; then
+    printf "%s\n" "[$(_c LIGHT_BLUE "Python - Remove")] $(_c LIGHT_RED "Python is not installed")"
+    return 1
+  fi
+
+  # check if no arguments are provided
+  if [ -z "$1" ]; then
+    printf "%s\n" "[$(_c LIGHT_BLUE "Python - Remove")] $(_c LIGHT_YELLOW "Usage: py.remove <package>")"
+    return 1
+  fi
+
+  # remove package
+  packages=$(printf "%s, " "$@" | sed 's/, $//')
+  printf "%s\n" "[$(_c LIGHT_BLUE "Python - Remove")] Removing $(_c LIGHT_YELLOW "$packages")"
+  pip uninstall "$@"
+}
