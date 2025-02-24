30ce58d6f7e53e8a2c1b247e1861079f07916c69
EnergyPatrikHU <energypatrikhu@proton.me>
2025-02-24 17:28:07 +0100

    Add d.up command for docker compose up

---
+++ commands/docker/up.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+d.up() {
+  if [[ $1 = "--help" ]] || [[ $1 = "-h" ]]; then
+    printf "%s\n" "[$(_c LIGHT_BLUE "Docker - Up")] $(_c LIGHT_YELLOW "Usage: d.up [all / container]")"
+    return
+  fi
+
+  if [ "$1" = "all" ]; then
+    if [[ ! -f "$EPX_PATH/.config/d.up.config" ]]; then
+      printf "%s\n" "[$(_c LIGHT_BLUE "Docker - Up")] $(_c LIGHT_RED "Config file not found, please create one at $EPX_PATH/.config/d.up.config")"
+      return
+    fi
+
+    . "$EPX_PATH/.config/d.up.config"
+
+    for d in "$CONTAINERS_DIR"/*; do
+      if [ -d "$d" ]; then
+        d.up "$(basename -- "$d")"
+      fi
+    done
+    return
+  fi
+
+  # check if container name is provided
+  if [[ -n $1 ]]; then
+    if [[ ! -f "$EPX_PATH/.config/d.up.config" ]]; then
+      printf "%s\n" "[$(_c LIGHT_BLUE "Docker - Up")] $(_c LIGHT_RED "Config file not found, please create one at $EPX_PATH/.config/d.up.config")"
+      return
+    fi
+
+    . "$EPX_PATH/.config/d.up.config"
+
+    fbasename=$(basename -- "$1")
+    dirname="/$CONTAINERS_DIR/$fbasename"
+
+    docker compose -f "$dirname/docker-compose.yml" pull
+    printf "\n"
+
+    docker compose -p "$fbasename" -f "$dirname/docker-compose.yml" up -d
+    printf "\n"
+    return
+  fi
+
+  # if nothing is provided, just start compose file in current directory
+  if [[ ! -f "docker-compose.yml" ]]; then
+    printf "%s\n" "[$(_c LIGHT_BLUE "Docker - Up")] $(_c LIGHT_RED "docker-compose.yml not found in current directory")"
+    return
+  fi
+
+  docker compose -f docker-compose.yml pull
+  printf "\n"
+
+  fbasename=$(basename -- "$(pwd)")
+
+  docker compose -p "$fbasename" -f docker-compose.yml up -d
+  printf "\n"
+}
+
+if [[ -f "$EPX_PATH/.config/d.up.config" ]]; then
+  _d.up_autocomplete() {
+    . $EPX_PATH/commands/docker/_autocomplete.sh
+    _autocomplete "$(dir "$CONTAINERS_DIR")"
+  }
+  complete -F _d.up_autocomplete d.up
+fi
