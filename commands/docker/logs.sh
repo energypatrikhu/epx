436128e21b451e3503ab76f83725579ab2a6f1c6
EnergyPatrikHU <energypatrikhu@proton.me>
2025-03-02 15:27:25 +0100

    Update usage message in d.logs command to include --all option

--- commands/docker/logs.sh
+++ commands/docker/logs.sh
@@ -2,11 +2,11 @@
 
 d.logs() {
   if [[ -z $1 ]]; then
-    printf "%s\n" "[$(_c LIGHT_BLUE "Docker - Logs")] $(_c LIGHT_YELLOW "Usage: d.logs <container> [--start | -s]")"
+    printf "%s\n" "[$(_c LIGHT_BLUE "Docker - Logs")] $(_c LIGHT_YELLOW "Usage: d.logs <container> [--all | -a]")"
     return
   fi
 
-  if [[ $2 = "--start" ]] || [[ $2 = "-s" ]]; then
+  if [[ ! $2 = "--all" ]] && [[ ! $2 = "-a" ]]; then
     docker container logs -f "$1" --since "$(docker inspect "$1" | jq .[0].State.StartedAt | sed 's/\"//g')"
     return
   fi
