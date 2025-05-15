9693235d5daca6ac25381ffe5acb51b01fa8ada3
energypatrikhu <energypatrikhu@proton.me>
2025-05-15 03:55:50 +0200

    Refactor d.stop function to replace __epx_echo with printf for consistent output formatting

--- commands/docker/stop.sh
+++ commands/docker/stop.sh
@@ -2,14 +2,14 @@
 
 d.stop() {
   if [[ -z $1 ]]; then
-    __epx_echo "[$(_c LIGHT_BLUE "Docker - Stop")] $(_c LIGHT_YELLOW "Usage: d.stop <all / container>")"
+    printf "[%s] %s\n" "$(_c LIGHT_BLUE "Docker - Stop")" "$(_c LIGHT_YELLOW "Usage: d.stop <all / container>")"
     return
   fi
 
   if [[ $1 == "all" ]]; then
-    __epx_echo "[$(_c LIGHT_BLUE "Docker - Stop")] $(_c LIGHT_RED "Stopping all containers...")"
+    printf "[%s] %s\n" "$(_c LIGHT_BLUE "Docker - Stop")" "$(_c LIGHT_RED "Stopping all containers...")"
     docker container stop $(docker ps -aq) >/dev/null 2>&1
-    __epx_echo "[$(_c LIGHT_BLUE "Docker - Stop")] $(_c LIGHT_RED "All containers stopped")"
+    printf "[%s] %s\n" "$(_c LIGHT_BLUE "Docker - Stop")" "$(_c LIGHT_RED "All containers stopped")"
   else
     if [ $# -eq 1 ]; then
       container_text="Container"
@@ -18,9 +18,9 @@ d.stop() {
     fi
     containers=$(printf "%s, " "$@" | sed 's/, $//')
 
-    __epx_echo "[$(_c LIGHT_BLUE "Docker - Stop")] $container_text $(_c LIGHT_BLUE "$containers") $(_c LIGHT_RED "stopping...")"
+    printf "[%s] %s\n" "$(_c LIGHT_BLUE "Docker - Stop")" "$container_text $(_c LIGHT_BLUE "$containers") $(_c LIGHT_RED "stopping...")"
     docker container stop "$@" >/dev/null 2>&1
-    __epx_echo "[$(_c LIGHT_BLUE "Docker - Stop")] $container_text $(_c LIGHT_BLUE "$containers") $(_c LIGHT_RED "stopped")"
+    printf "[%s] %s\n" "$(_c LIGHT_BLUE "Docker - Stop")" "$container_text $(_c LIGHT_BLUE "$containers") $(_c LIGHT_RED "stopped")"
   fi
 }
 
