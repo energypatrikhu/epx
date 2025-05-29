fb9ba20f1a9af82ae354068787e56e878f51892f
energypatrikhu <energypatrikhu@proton.me>
2025-05-29 08:30:56 +0200

    Add d.net and d.network functions for Docker network management

---
+++ commands/docker/network.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+d.net() {
+  if [[ -z $1 ]]; then
+    __epx_echo "[$(_c LIGHT_BLUE "Docker - Network")] $(_c LIGHT_YELLOW "Usage: d.net <... options>")"
+    return
+  fi
+
+  docker network "$@"
+}
+
+d.network() {
+  d.net $@
+}
+
+. "$EPX_PATH/commands/docker/_autocomplete.sh"
+complete -F _d_autocomplete d.net
+complete -F _d_autocomplete d.network
