c4af8ccd293111fa4fd4879d96466691c1d503fe
energypatrikhu <energypatrikhu@proton.me>
2025-07-08 15:20:18 +0200

    Refactor d.prune function to handle multiple prune options with usage instructions

--- commands/docker/prune.sh
+++ commands/docker/prune.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
 
 d.prune() {
-  docker system prune -a --volumes
+  if [[ -z $1 ]]; then
+    __epx_echo "[$(_c LIGHT_BLUE "Docker - Prune")] $(_c LIGHT_YELLOW "Usage: d.prune <all / images / containers / volumes / networks>")"
+    return
+  fi
+
+  local args=("$@")
+
+  case $1 in
+  all)
+    __epx_echo "[$(_c LIGHT_BLUE "Docker - Prune")] $(_c LIGHT_CYAN "Pruning all unused Docker resources...")"
+    docker system prune --all --volumes ${args[@]:1}
+    ;;
+  images)
+    __epx_echo "[$(_c LIGHT_BLUE "Docker - Prune")] $(_c LIGHT_CYAN "Pruning unused Docker images...")"
+    docker image prune --all ${args[@]:1}
+    ;;
+  containers)
+    __epx_echo "[$(_c LIGHT_BLUE "Docker - Prune")] $(_c LIGHT_CYAN "Pruning stopped Docker containers...")"
+    docker container prune ${args[@]:1}
+    ;;
+  volumes)
+    __epx_echo "[$(_c LIGHT_BLUE "Docker - Prune")] $(_c LIGHT_CYAN "Pruning unused Docker volumes...")"
+    docker volume prune ${args[@]:1}
+    ;;
+  networks)
+    __epx_echo "[$(_c LIGHT_BLUE "Docker - Prune")] $(_c LIGHT_CYAN "Pruning unused Docker networks...")"
+    docker network prune ${args[@]:1}
+    ;;
+  *)
+    __epx_echo "[$(_c LIGHT_BLUE "Docker - Prune")] $(_c LIGHT_RED "Unknown option: $1")"
+    return 1
+    ;;
+  esac
 }
