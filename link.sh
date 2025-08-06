f9f5ae1fb273d38b79d57f8d7e1766a257868407
energypatrikhu <energypatrikhu@proton.me>
2025-08-06 21:14:24 +0200

    Update broken symlink removal message for clarity

--- link.sh
+++ link.sh
@@ -67,13 +67,23 @@ _load_functions() {
 }
 
 # Find and remove broken symlinks in /usr/local/bin that point to /usr/local/epx/scripts
+# find /usr/local/bin -maxdepth 1 -type l -exec sh -c '
+#   for link; do
+#     target=$(readlink "$link")
+#     if [ "${target#${EPX_HOME}/scripts/}" != "$target" ]; then
+#       if ! [ -e "$target" ]; then
+#         echo "Removing broken symlink: $link -> $target"
+#         rm "$link"
+#       fi
+#     fi
+#   done
+# ' sh {} +
 find /usr/local/bin -maxdepth 1 -type l -exec sh -c '
   for link; do
     target=$(readlink "$link")
     if [ "${target#${EPX_HOME}/scripts/}" != "$target" ]; then
       if ! [ -e "$target" ]; then
-        echo "Removing broken symlink: $link -> $target"
-        rm "$link"
+        echo "Broken symlink: $link -> $target"
       fi
     fi
   done
