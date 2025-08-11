3ec809f5066bc575d5c2015e9a83d6a5dc010f5e
energypatrikhu <energypatrikhu@proton.me>
2025-08-11 23:09:51 +0200

    Fix virtual environment activation to remove unnecessary directory change

--- commands/python/py.venv.sh
+++ commands/python/py.venv.sh
@@ -36,10 +36,10 @@ if [ ! -d .venv ]; then
 
   # activate virtual environment
   echo -e "[$(_c LIGHT_BLUE "Python - VENV")] Activating virtual environment"
-  exec bash -i -c "cd "$PWD" && source .venv/bin/activate"
+  exec bash -i -c "source .venv/bin/activate"
   exit 0
 fi
 
 # activate virtual environment
 echo -e "[$(_c LIGHT_BLUE "Python - VENV")] Activating virtual environment"
-exec bash -i -c "cd "$PWD" && source .venv/bin/activate"
+exec bash -i -c ".venv/bin/activate"
