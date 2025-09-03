9be8d55bc670f2fe6248db3b41a55dd9598b2b54
energypatrikhu <energypatrikhu@proton.me>
2025-09-03 15:54:54 +0200

    Refactor: Update autocomplete options to include mk-cert command

--- autocomplete.sh
+++ autocomplete.sh
@@ -21,6 +21,6 @@ _load_autocomplete() {
 _load_autocomplete "${EPX_HOME}/commands"
 
 _epx_completions() {
-  _autocomplete "self-update update-bees backup"
+  _autocomplete "self-update mk-cert update-bees backup"
 }
 complete -F _epx_completions epx
