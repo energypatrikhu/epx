c3dda6e1d62b1821d5bde4bc5a60dfed70ad0027
energypatrikhu <energypatrikhu@proton.me>
2025-09-26 11:09:11 +0200

    Refactor: Enhance help command and option handling in d.pull.sh

--- commands/docker/d.pull.sh
+++ commands/docker/d.pull.sh
@@ -1,19 +1,41 @@
 _cci docker
 
-if [[ "${*}" =~ ^-*h(elp)?$ ]]; then
+help() {
   echo -e "[$(_c LIGHT_BLUE "Docker - Pull")] $(_c LIGHT_YELLOW "Usage: d.pull [<options>] [all / [container1, container2, ...]]")"
   echo -e "[$(_c LIGHT_BLUE "Docker - Pull")] $(_c LIGHT_YELLOW "Options:")"
-  echo -e "[$(_c LIGHT_BLUE "Docker - Pull")] $(_c LIGHT_YELLOW "  all") $(_c LIGHT_GREEN "Pull all containers defined in the config file")"
+  echo -e "[$(_c LIGHT_BLUE "Docker - Pull")] $(_c LIGHT_YELLOW "  -a | --all") $(_c LIGHT_GREEN "Pull all containers defined in the config file")"
+  echo -e "[$(_c LIGHT_BLUE "Docker - Pull")] $(_c LIGHT_YELLOW "  -h | --help") $(_c LIGHT_GREEN "Show this help message")"
   echo -e "[$(_c LIGHT_BLUE "Docker - Pull")] $(_c LIGHT_YELLOW "  [container1, container2, ...]") $(_c LIGHT_GREEN "Pull specific containers by name")"
   echo -e "[$(_c LIGHT_BLUE "Docker - Pull")] $(_c LIGHT_YELLOW "  If no arguments are provided, it will pull the compose file in the current directory")"
   echo -e "[$(_c LIGHT_BLUE "Docker - Pull")] $(_c LIGHT_YELLOW "  If the config file is not found, it will prompt to create one at ${EPX_HOME}/.config/docker.config")"
+}
+
+opt_help=false
+opt_all=false
+
+for arg in "$@"; do
+  if [[ "${arg}" == -* ]]; then
+    if [[ "${arg}" =~ ^-*h(elp)?$ ]]; then
+      opt_help=true
+    elif [[ "${arg}" =~ ^-*a(ll)?$ ]]; then
+      opt_all=true
+    else
+      echo -e "[$(_c LIGHT_BLUE "Docker - Pull")] $(_c LIGHT_RED "Unknown option: ${arg}")"
+      help
+      exit 1
+    fi
+  fi
+done
+
+if [[ "${opt_help}" == "true" ]]; then
+  help
   exit
 fi
 
-if [[ "${1-}" = "all" ]]; then
+if [[ "${opt_all}" == "true" ]]; then
   if [[ ! -f "${EPX_HOME}/.config/docker.config" ]]; then
     echo -e "[$(_c LIGHT_BLUE "Docker - Pull")] $(_c LIGHT_RED "Config file not found, please create one at ${EPX_HOME}/.config/docker.config")"
-    echo -e "[$(_c LIGHT_BLUE "Docker - Pull")] $(_c LIGHT_RED "Use 'd.pull --help' for more information")"
+    help
     exit
   fi
 
@@ -33,7 +55,7 @@ fi
 if [[ -n $* ]]; then
   if [[ ! -f "${EPX_HOME}/.config/docker.config" ]]; then
     echo -e "[$(_c LIGHT_BLUE "Docker - Pull")] $(_c LIGHT_RED "Config file not found, please create one at ${EPX_HOME}/.config/docker.config")"
-    echo -e "[$(_c LIGHT_BLUE "Docker - Pull")] $(_c LIGHT_RED "Use 'd.pull --help' for more information")"
+    help
     exit
   fi
 
@@ -56,7 +78,7 @@ fi
 # if nothing is provided, just start compose file in current directory
 if [[ ! -f "docker-compose.yml" ]]; then
   echo -e "[$(_c LIGHT_BLUE "Docker - Pull")] $(_c LIGHT_RED "docker-compose.yml not found in current directory")"
-  echo -e "[$(_c LIGHT_BLUE "Docker - Pull")] $(_c LIGHT_RED "Use 'd.pull --help' for more information")"
+  help
   exit
 fi
 
