1693e0f47e508b3679ba6bb631a9140bcf0c7538
energypatrikhu <energypatrikhu@proton.me>
2025-10-14 16:55:34 +0200

    Fix: Update usage message in d.prune.sh to include 'build' option and remove unnecessary flags from prune commands

--- commands/docker/d.prune.sh
+++ commands/docker/d.prune.sh
@@ -1,18 +1,18 @@
 _cci docker
 
 if [[ -z "${1-}" ]]; then
-  echo -e "[$(_c LIGHT_BLUE "Docker - Prune")] $(_c LIGHT_YELLOW "Usage: d.prune <all / images / containers / volumes / networks> [options]")"
+  echo -e "[$(_c LIGHT_BLUE "Docker - Prune")] $(_c LIGHT_YELLOW "Usage: d.prune <all / images / containers / volumes / networks / build> [options]")"
   exit
 fi
 
 case "${1-}" in
 all)
   echo -e "[$(_c LIGHT_BLUE "Docker - Prune")] $(_c LIGHT_CYAN "Pruning all unused Docker resources...")"
-  docker system prune --all --volumes "${@:2}"
+  docker system prune --volumes "${@:2}"
   ;;
 images)
   echo -e "[$(_c LIGHT_BLUE "Docker - Prune")] $(_c LIGHT_CYAN "Pruning unused Docker images...")"
-  docker image prune --all "${@:2}"
+  docker image prune "${@:2}"
   ;;
 containers)
   echo -e "[$(_c LIGHT_BLUE "Docker - Prune")] $(_c LIGHT_CYAN "Pruning stopped Docker containers...")"
@@ -26,8 +26,12 @@ networks)
   echo -e "[$(_c LIGHT_BLUE "Docker - Prune")] $(_c LIGHT_CYAN "Pruning unused Docker networks...")"
   docker network prune "${@:2}"
   ;;
+build)
+  echo -e "[$(_c LIGHT_BLUE "Docker - Prune")] $(_c LIGHT_CYAN "Pruning unused Docker builder cache...")"
+  docker buildx prune "${@:2}"
+  ;;
 *)
-  echo -e "[$(_c LIGHT_BLUE "Docker - Prune")] $(_c LIGHT_RED "Unknown option: ${1-}")"
+  echo -e "[$(_c LIGHT_BLUE "Docker - Prune")] $(_c LIGHT_RED "Unknown option:") ${1-}"
   exit 1
   ;;
 esac
