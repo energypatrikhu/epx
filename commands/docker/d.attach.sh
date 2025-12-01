e640c1f2e30f28a9c227cd13775a48c045fe6508
energypatrikhu <energypatrikhu@proton.me>
2025-12-01 11:12:56 +0100

    Fix: Enhance user interaction for Docker attach command with confirmation prompt

--- commands/docker/d.attach.sh
+++ commands/docker/d.attach.sh
@@ -5,4 +5,13 @@ if [[ -z $* ]]; then
   exit 1
 fi
 
-docker container attach --sig-proxy=false --detach-keys="ctrl-c" "${@}"
+echo -e "[$(_c LIGHT_BLUE "Docker - Attach")] $(_c LIGHT_YELLOW "Warning: Attaching to container ${@}")"
+echo -e "[$(_c LIGHT_BLUE "Docker - Attach")] $(_c LIGHT_YELLOW "To detach: Press Ctrl+P followed by Ctrl+Q")"
+read -p "Continue? (y/N): " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+  echo -e "[$(_c LIGHT_BLUE "Docker - Attach")] $(_c LIGHT_RED "Aborted")"
+  exit 0
+fi
+
+docker container attach "${@}"
