0162a7ea219a4ffc4d8589ca4b0f904fad326dcd
energypatrikhu <energypatrikhu@proton.me>
2025-12-01 11:15:13 +0100

    Fix: Correct warning message formatting in Docker attach command

--- commands/docker/d.attach.sh
+++ commands/docker/d.attach.sh
@@ -5,7 +5,7 @@ if [[ -z $* ]]; then
   exit 1
 fi
 
-echo -e "[$(_c LIGHT_BLUE "Docker - Attach")] $(_c LIGHT_YELLOW "Warning: Attaching to container ${@}")"
+echo -e "[$(_c LIGHT_BLUE "Docker - Attach")] $(_c LIGHT_YELLOW "Warning: Attaching to container") ${@}"
 echo -e "[$(_c LIGHT_BLUE "Docker - Attach")] $(_c LIGHT_YELLOW "To detach: Press") Ctrl+P $(_c LIGHT_YELLOW "followed by") Ctrl+Q"
 read -p "Continue? (y/N): " -n 1 -r
 echo
