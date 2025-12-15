5994cc88c5658047c75a148ca3a5d32bd355d9fd
energypatrikhu <energypatrikhu@proton.me>
2025-12-15 05:02:40 +0100

    Add: Implement autocomplete for docker-compose commands in LinuxGSM

--- commands/game-servers/linuxgsm/_autocomplete.sh
+++ commands/game-servers/linuxgsm/_autocomplete.sh
@@ -7,3 +7,25 @@ complete -F _gsm_autocomplete gsm
 complete -F _gsm_autocomplete gsm.start
 complete -F _gsm_autocomplete gsm.stop
 complete -F _gsm_autocomplete gsm.rm
+
+_gsm_autocomplete_compose() {
+  . "${EPX_HOME}/.config/docker.config"
+
+  local container_dirs=()
+  local d
+  for d in "${CONTAINERS_DIR}"/*; do
+    if [[ -d "${d}" ]]; then
+      if [[ -f "${d}/docker-compose.yml" ]]; then
+        local dirname="$(basename -- "${d}")"
+        if [[ "${dirname}" == linuxgsm-* ]]; then
+          container_dirs+=("${dirname#linuxgsm-}")
+        fi
+      fi
+    fi
+  done
+
+  _autocomplete "${container_dirs[@]}"
+}
+if [[ -f "${EPX_HOME}/.config/docker.config" ]]; then
+  complete -F _d_autocomplete_compose gsm.up
+fi
