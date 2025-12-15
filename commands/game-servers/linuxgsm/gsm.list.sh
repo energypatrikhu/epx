595d2c3492662159fcd182cfcaff0b96fd416fb7
energypatrikhu <energypatrikhu@proton.me>
2025-12-15 20:37:35 +0100

    Fix: Enhance filtering logic for game server search in gsm.list.sh

--- commands/game-servers/linuxgsm/gsm.list.sh
+++ commands/game-servers/linuxgsm/gsm.list.sh
@@ -14,7 +14,7 @@ available_servers="$(curl -sL https://raw.githubusercontent.com/GameServerManage
 opt_find=""
 if [[ -n "${1-}" ]]; then
   opt_find="${1}"
-  available_servers="$(echo "${available_servers}" | grep -iE "^${opt_find},")"
+  available_servers="$(echo "${available_servers}" | grep -iE "(^|,|[^a-z])${opt_find}([^a-z]|,|$)")"
 fi
 
 echo -e "[$(_c LIGHT_BLUE "LinuxGSM")] $(_c LIGHT_GREEN "Available game servers:")"
