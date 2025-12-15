af83c0c1f92220d86ec005aa047651aea7f89069
energypatrikhu <energypatrikhu@proton.me>
2025-12-15 04:42:21 +0100

    Add: Implement gsm.add.sh script for adding game servers

---
+++ commands/game-servers/linuxgsm/gsm.add.sh
@@ -0,0 +1,20 @@
+_cci docker
+
+if [[ ! -f "${EPX_HOME}/.config/docker.config" ]]; then
+  echo -e "[$(_c LIGHT_BLUE "LinuxGSM")] $(_c LIGHT_RED "Config file not found, please create one at") ${EPX_HOME}/.config/docker.config"
+  exit
+fi
+
+. "${EPX_HOME}/.config/docker.config"
+
+game_server_tag="${1-}"
+if [[ -z "${game_server_tag}" ]]; then
+  echo "Usage: gm-add <game-server-tag>"
+  echo "Example: gm-add cs2"
+  echo "Available game server tags can be found at: https://github.com/GameServerManagers/LinuxGSM/blob/master/lgsm/data/serverlist.csv"
+  exit 1
+fi
+
+mkdir -p "${CONTAINERS_DIR}/${game_server_tag}"
+cp "${EPX_HOME}/templates/linuxgsm/docker-compose.template" "${CONTAINERS_DIR}/${game_server_tag}/docker-compose.yml"
+sed -i "s/TAG/${game_server_tag}/g" "${CONTAINERS_DIR}/${game_server_tag}/docker-compose.yml"
