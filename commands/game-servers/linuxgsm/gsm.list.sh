b6e425f0d60be2d987527b37486125f7bfadf96c
energypatrikhu <energypatrikhu@proton.me>
2025-12-15 20:29:01 +0100

    Add: Create gsm.list.sh script to list available game servers

---
+++ commands/game-servers/linuxgsm/gsm.list.sh
@@ -0,0 +1,17 @@
+# shortname,gameservername,gamename,os
+# ac,acserver,Assetto Corsa,ubuntu-24.04
+# ahl,ahlserver,Action Half-Life,ubuntu-24.04
+# ahl2,ahl2server,Action: Source,ubuntu-24.04
+
+# List of available game servers
+# Example:
+# Game servers:
+#   Assetto Corsa (ac)
+#   Action Half-Life (ahl)
+#   Action: Source (ahl2)
+
+available_servers="$(curl -sL https://raw.githubusercontent.com/GameServerManagers/LinuxGSM/refs/heads/master/lgsm/data/serverlist.csv)"
+echo -e "[$(_c LIGHT_BLUE "LinuxGSM")] $(_c LIGHT_GREEN "Available game servers:")"
+echo "${available_servers}" | tail -n +2 | while IFS=, read -r shortname gameservername gamename os; do
+  echo -e "  $(_c LIGHT_YELLOW "${gamename} (${shortname})")"
+done
