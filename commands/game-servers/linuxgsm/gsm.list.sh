474eeaaeedde1648f61b6abc850a1f0d55a5116e
energypatrikhu <energypatrikhu@proton.me>
2025-12-15 21:05:52 +0100

    Fix: Refine available game servers filtering logic in gsm.list.sh

--- commands/game-servers/linuxgsm/gsm.list.sh
+++ commands/game-servers/linuxgsm/gsm.list.sh
@@ -10,16 +10,14 @@
 #   Action Half-Life (ahl)
 #   Action: Source (ahl2)
 
-available_servers="$(curl -sL https://raw.githubusercontent.com/GameServerManagers/LinuxGSM/refs/heads/master/lgsm/data/serverlist.csv)"
+available_servers="$(curl -sL https://raw.githubusercontent.com/GameServerManagers/LinuxGSM/refs/heads/master/lgsm/data/serverlist.csv | awk -F',' '{print $1","$3}' | tail -n +2)"
 opt_find=""
 if [[ -n "${1-}" ]]; then
   opt_find="${1}"
-  available_servers="$(echo "${available_servers}" | awk -F',' -v search="${opt_find}" 'NR==1 {print $1","$3; next} tolower($1) ~ tolower(search) || tolower($3) ~ tolower(search) {print $1","$3}')"
-else
-  available_servers="$(echo "${available_servers}" | awk -F',' '{print $1","$3}')"
+  available_servers="$(echo "${available_servers}" | awk -F',' -v search="${opt_find}" 'tolower($1) ~ tolower(search) || tolower($3) ~ tolower(search) {print $1","$3}')"
 fi
 
 echo -e "[$(_c LIGHT_BLUE "LinuxGSM")] $(_c LIGHT_GREEN "Available game servers:")"
-echo "${available_servers}" | tail -n +2 | while IFS=, read -r shortname gamename; do
+echo "${available_servers}" | while IFS=, read -r shortname gamename; do
   echo -e "  $(_c LIGHT_YELLOW "${gamename}") $(_c LIGHT_CYAN "(${shortname})")"
 done
