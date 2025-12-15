98ac9b02b85b34a61ce1c2bb5b19beed31cf31a9
energypatrikhu <energypatrikhu@proton.me>
2025-12-15 04:44:48 +0100

    Fix: Update usage instructions in gsm.sh script for clarity

--- commands/game-servers/linuxgsm/gsm.sh
+++ commands/game-servers/linuxgsm/gsm.sh
@@ -2,9 +2,9 @@ _cci docker
 
 game_server_name="${1-}"
 if [[ -z "${game_server_name}" ]]; then
-  echo "Usage: gm <game-server-name> <command>"
-  echo "Example: gm cs2server start"
+  echo "Usage: gsm <game-server-name> <command>"
+  echo "Example: gsm cs2server start"
   exit 1
 fi
 
-docker exec -it --user linuxgsm ${game_server_name} ./${game_server_name} ${*:2}
+docker exec -it --user linuxgsm gsm-${game_server_name} ./${game_server_name} ${*:2}
