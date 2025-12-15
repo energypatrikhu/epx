ef22eb3424538f89fa68218aafa44934103e0b24
energypatrikhu <energypatrikhu@proton.me>
2025-12-15 21:18:03 +0100

    Fix: Update usage examples in gsm.sh for consistency and clarity

--- commands/game-servers/linuxgsm/gsm.sh
+++ commands/game-servers/linuxgsm/gsm.sh
@@ -5,11 +5,12 @@ if [[ -z "${game_server_name}" ]]; then
   echo -e "[$(_c LIGHT_BLUE "LinuxGSM")] $(_c LIGHT_RED "Usage:") gsm <game-server-tag> <command>"
   echo -e "[$(_c LIGHT_BLUE "LinuxGSM")] $(_c LIGHT_YELLOW "Examples:")"
   echo -e "  $(_c LIGHT_GREEN "gsm") $(_c WHITE "cs2") $(_c LIGHT_GREEN "start")"
-  echo -e "  $(_c LIGHT_RED "gsm") $(_c WHITE "cs2") $(_c LIGHT_RED "stop")"
-  echo -e "  $(_c LIGHT_YELLOW "gsm") $(_c WHITE "cs2") $(_c LIGHT_YELLOW "restart")"
-  echo -e "  $(_c LIGHT_CYAN "gsm") $(_c WHITE "cs2") $(_c LIGHT_CYAN "status")"
-  echo -e "  $(_c LIGHT_PURPLE "gsm") $(_c WHITE "cs2") $(_c LIGHT_PURPLE "console")"
-  echo -e "  $(_c LIGHT_BLUE "gsm") $(_c WHITE "cs2") $(_c LIGHT_BLUE "details")"
+  echo -e "  $(_c LIGHT_RED "gsm") $(_c WHITE "tf2") $(_c LIGHT_RED "stop")"
+  echo -e "  $(_c LIGHT_YELLOW "gsm") $(_c WHITE "gmod") $(_c LIGHT_YELLOW "restart")"
+  echo -e "  $(_c LIGHT_CYAN "gsm") $(_c WHITE "terraria") $(_c LIGHT_CYAN "status")"
+  echo -e "  $(_c LIGHT_PURPLE "gsm") $(_c WHITE "valheim") $(_c LIGHT_PURPLE "console")"
+  echo -e "  $(_c LIGHT_BLUE "gsm") $(_c WHITE "rust") $(_c LIGHT_BLUE "details")"
+
 
 
   exit 1
