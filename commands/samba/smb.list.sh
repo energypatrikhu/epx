856ce6f24def371b97ec2931448c1989a4d69864
energypatrikhu <energypatrikhu@proton.me>
2025-12-27 00:00:18 +0100

    Add: Implement smb.list.sh script to list available Samba shares and update smb.help.sh with command information

---
+++ commands/samba/smb.list.sh
@@ -0,0 +1,17 @@
+_cci grep sed
+
+echo -e "[$(_c LIGHT_BLUE "Samba - List Shares")] $(_c LIGHT_YELLOW "Listing available Samba shares from configuration...")"
+
+if [[ ! -f /etc/samba/smb.conf ]]; then
+  echo -e "[$(_c LIGHT_BLUE "Samba - List Shares")] $(_c LIGHT_RED "Samba configuration file not found at /etc/samba/smb.conf")"
+  exit 1
+fi
+
+shares=$(grep '^\[' /etc/samba/smb.conf | grep -v '^\[global\]' | sed 's/\[//' | sed 's/\]//' | sort)
+
+if [[ -z "${shares}" ]]; then
+  echo -e "[$(_c LIGHT_BLUE "Samba - List Shares")] $(_c LIGHT_YELLOW "No shares found in configuration")"
+else
+  echo -e "[$(_c LIGHT_BLUE "Samba - List Shares")] $(_c LIGHT_GREEN "Available shares:")"
+  echo "${shares}"
+fi
