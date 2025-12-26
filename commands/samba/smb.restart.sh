382e497175d092135b515a3b7ee9e1df0b1eef8c
energypatrikhu <energypatrikhu@proton.me>
2025-12-26 23:00:59 +0100

    Fix: Correct syntax error in smb.restart.sh for conditional checks

--- commands/samba/smb.restart.sh
+++ commands/samba/smb.restart.sh
@@ -2,7 +2,7 @@ _cci samba
 
 if command -v service &> /dev/null; then
   service smbd restart
-else if command -v systemctl &> /dev/null; then
+elif command -v systemctl &> /dev/null; then
   systemctl restart smbd
 else
   echo "Cannot restart Samba services: neither systemctl nor service command found."
