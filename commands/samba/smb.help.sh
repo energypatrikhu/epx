40093bc19aa22661637ad0af7a0ee953e923a8f8
energypatrikhu <energypatrikhu@proton.me>
2025-12-26 23:10:05 +0100

    Add: Create smb.help.sh script to provide help information for Samba commands

---
+++ commands/samba/smb.help.sh
@@ -0,0 +1,4 @@
+echo -e "[$(_c LIGHT_BLUE "Samba - Help")]"
+echo -e "  $(_c CYAN "Commands:")"
+echo -e "    $(_c LIGHT_CYAN "smb.restart") - Restart Samba service"
+echo -e "    $(_c LIGHT_CYAN "smb.help") - Display this help message"
