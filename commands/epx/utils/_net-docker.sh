edf637cfad6e6bc37a06697bf13f3402e30ff145
energypatrikhu <energypatrikhu@proton.me>
2025-12-27 12:10:56 +0100

    Fix: Comment out port mappings summary section in Docker networking script

--- commands/epx/utils/_net-docker.sh
+++ commands/epx/utils/_net-docker.sh
@@ -82,19 +82,19 @@ __epx_net_docker() {
     echo ""
   done
 
-  _print_section "PORT MAPPINGS SUMMARY"
+  # _print_section "PORT MAPPINGS SUMMARY"
 
-  # List all port mappings
-  docker ps --format '{{.Names}}\t{{.Ports}}' | while IFS=$'\t' read name ports; do
-    if [[ -n "$ports" ]]; then
-      # Extract host ports
-      echo "$ports" | grep -o '[0-9]*->' | while read port_map; do
-        # Remove '->' suffix using parameter expansion
-        local host_port="${port_map%->}"
-        printf "  Host:%-5s → %-47s\n" "$host_port" "$name"
-      done
-    fi
-  done
+  # # List all port mappings
+  # docker ps --format '{{.Names}}\t{{.Ports}}' | while IFS=$'\t' read name ports; do
+  #   if [[ -n "$ports" ]]; then
+  #     # Extract host ports
+  #     echo "$ports" | grep -o '[0-9]*->' | while read port_map; do
+  #       # Remove '->' suffix using parameter expansion
+  #       local host_port="${port_map%->}"
+  #       printf "  Host:%-5s → %-47s\n" "$host_port" "$name"
+  #     done
+  #   fi
+  # done
 
   _print_section "CONTAINER CONNECTIVITY"
 
