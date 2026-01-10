3021e9a22ad94cada15f920a4e8a54c033a4264c
energypatrikhu <energypatrikhu@proton.me>
2026-01-10 18:41:25 +0100

    Fix: Remove unnecessary line breaks in trash clearing success and error messages

--- commands/filesystem/fs.cleartrash.sh
+++ commands/filesystem/fs.cleartrash.sh
@@ -133,16 +133,13 @@ _clear_trash_dir() {
 
         if [[ $? -eq 0 ]]; then
           echo -e "[$(_c LIGHT_BLUE "FS - Clear Trash")] $(_c LIGHT_GREEN "Trash cleared successfully")"
-          echo ""
           return 0
         else
           echo -e "[$(_c LIGHT_BLUE "FS - Clear Trash")] $(_c LIGHT_RED "Error"): Failed to clear trash"
-          echo ""
           return 1
         fi
       else
         echo -e "[$(_c LIGHT_BLUE "FS - Clear Trash")] Skipped trash at $(_c LIGHT_YELLOW "$trash_path")"
-        echo ""
         return 0
       fi
     else
