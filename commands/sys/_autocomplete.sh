be1e1eb149570464e52cdb300930c256ea33993d
energypatrikhu <energypatrikhu@proton.me>
2026-03-13 19:33:28 +0100

    Rename autocomplete function for system services to improve clarity

--- commands/sys/_autocomplete.sh
+++ commands/sys/_autocomplete.sh
@@ -1,4 +1,4 @@
-__epx_sys_services() {
+__epx_bash_sys_services() {
   local services
   if command -v systemctl &> /dev/null; then
     services="$(systemctl list-units --type=service --all --no-legend --plain | awk '{print $1}' | sed 's/\.service$//' | sort -u)"
@@ -7,10 +7,10 @@ __epx_sys_services() {
   fi
   _autocomplete "${services}"
 }
-complete -F __epx_sys_services sys.disable
-complete -F __epx_sys_services sys.enable
-complete -F __epx_sys_services sys.remove
-complete -F __epx_sys_services sys.restart
-complete -F __epx_sys_services sys.start
-complete -F __epx_sys_services sys.status
-complete -F __epx_sys_services sys.stop
+complete -F __epx_bash_sys_services sys.disable
+complete -F __epx_bash_sys_services sys.enable
+complete -F __epx_bash_sys_services sys.remove
+complete -F __epx_bash_sys_services sys.restart
+complete -F __epx_bash_sys_services sys.start
+complete -F __epx_bash_sys_services sys.status
+complete -F __epx_bash_sys_services sys.stop
