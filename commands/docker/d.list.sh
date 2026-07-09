df5d0e1050c02d3c70da6330be519cd3536f028f
energypatrikhu <energypatrikhu@proton.me>
2026-07-09 20:58:58 +0200

    add option --small

--- commands/docker/d.list.sh
+++ commands/docker/d.list.sh
@@ -1,9 +1,10 @@
 _help() {
-  echo -e "[$(_c LIGHT_BLUE "Docker - List")] Usage: $(_c LIGHT_YELLOW "d.list [status]")"
+  echo -e "[$(_c LIGHT_BLUE "Docker - List")] Usage: $(_c LIGHT_YELLOW "d.list [status] [--small|-s]")"
   echo -e "[$(_c LIGHT_BLUE "Docker - List")] List Docker containers with optional status filtering"
   echo -e "[$(_c LIGHT_BLUE "Docker - List")]"
   echo -e "[$(_c LIGHT_BLUE "Docker - List")] Options:"
   echo -e "[$(_c LIGHT_BLUE "Docker - List")]   -h, --help        Show this help message and exit"
+  echo -e "[$(_c LIGHT_BLUE "Docker - List")]   -s, --small       Show minimal output with only name and status"
   echo -e "[$(_c LIGHT_BLUE "Docker - List")]"
   echo -e "[$(_c LIGHT_BLUE "Docker - List")] Status Filters:"
   echo -e "[$(_c LIGHT_BLUE "Docker - List")]   created"
@@ -17,14 +18,17 @@ _help() {
   echo -e "[$(_c LIGHT_BLUE "Docker - List")] Examples:"
   echo -e "[$(_c LIGHT_BLUE "Docker - List")]   d.list"
   echo -e "[$(_c LIGHT_BLUE "Docker - List")]   d.list running"
-  echo -e "[$(_c LIGHT_BLUE "Docker - List")]   d.list exited paused"
+  echo -e "[$(_c LIGHT_BLUE "Docker - List")]   d.list exited paused --small"
 }
 
 opt_help=false
+opt_small=false
 for arg in "$@"; do
   if [[ "${arg}" == -* ]]; then
     if [[ "${arg}" =~ ^-*h(elp)?$ ]]; then
       opt_help=true
+    elif [[ "${arg}" =~ ^-*s(mall)?$ ]]; then
+      opt_small=true
     fi
   fi
 done
@@ -37,12 +41,10 @@ fi
 _cci_pkg docker:docker-ce-cli
 
 filters=""
-if [[ -n $* ]]; then
   for filter in "${@}"; do
+  if [[ "${filter}" =~ ^- ]]; then continue; fi
     filters="${filters} --filter status=${filter}"
   done
-fi
-
 data=$(docker ps -a --format "{{.ID}}\t{{.Names}}\t{{.Image}}\t{{.Status}}" ${filters})
 
 if [[ -z "${data}" ]]; then
@@ -50,45 +52,60 @@ if [[ -z "${data}" ]]; then
   exit
 fi
 
-id_width=12
-names_width=5
-image_width=5
-status_width=7
-
-while IFS=$'\t' read -r id names image status; do
-  id_width=$((${#id} > id_width ? ${#id} : id_width))
-  names_width=$((${#names} > names_width ? ${#names} : names_width))
-  image_width=$((${#image} > image_width ? ${#image} : image_width))
-  status_width=$((${#status} > status_width ? ${#status} : status_width))
-done <<EOF
-${data}
-EOF
-
-names_width=$((names_width + 2))
-separator=$(printf "+%-${id_width}s--+%-${names_width}s--+%-${image_width}s--+%-${status_width}s--+\n" | tr ' ' '-')
-
-echo -e "${separator}"
-printf "| %-${id_width}s | %-${names_width}s | %-${image_width}s | %-${status_width}s |\n" "CONTAINER ID" "NAME" "IMAGE" "STATUS"
-echo -e "${separator}"
-
-# Sort data by the second column (names)
-sorted_data=$(printf "%s\n" "${data}" | sort -k2,2)
-
-while IFS=$'\t' read -r id names image status; do
-
+if [[ "${opt_small}" == "true" ]]; then
+  for line in ${data}; do
+    id=$(echo ${line} | cut -f1)
+    names=$(echo ${line} | cut -f2)
+    status=$(echo ${line} | cut -f4)
   if printf "%s" "${status}" | grep -q "Up"; then
     bullet=$(_c GREEN "${EPX_BULLET}")
   else
     bullet=$(_c RED "${EPX_BULLET}")
   fi
 
-  names="${bullet} ${names}"
-  visible_names_length=$(printf "%s" "${names}" | sed 's/\x1b\[[0-9;]*m//g' | wc -m)
-  padding=$((names_width - visible_names_length))
+    echo "| ${bullet} ${names}\t\t| ${status}"
+  done
+else
+  id_width=12
+  names_width=5
+  image_width=5
+  status_width=7
 
-  printf "| %-${id_width}s | %s%-${padding}s | %-${image_width}s | %-${status_width}s |\n" "${id}" "${names}" "" "${image}" "${status}"
+  while IFS=$'\t' read -r id names image status; do
+    id_width=$((${#id} > id_width ? ${#id} : id_width))
+    names_width=$((${#names} > names_width ? ${#names} : names_width))
+    image_width=$((${#image} > image_width ? ${#image} : image_width))
+    status_width=$((${#status} > status_width ? ${#status} : status_width))
 done <<EOF
+${data}
+EOF
+
+  names_width=$((names_width + 2))
+  separator=$(printf "+%-${id_width}s--+%-${names_width}s--+%-${image_width}s--+%-${status_width}s--+\n" | tr ' ' '-')
+
+echo -e "${separator}"
+  printf "| %-${id_width}s | %-${names_width}s | %-${image_width}s | %-${status_width}s |\n" "CONTAINER ID" "NAME" "IMAGE" "STATUS"
+  echo -e "${separator}"
+
+  # Sort data by the second column (names)
+  sorted_data=$(printf "%s\n" "${data}" | sort -k2,2)
+
+while IFS=$'\t' read -r id names image status; do
+
+    if printf "%s" "${status}" | grep -q "Up"; then
+      bullet=$(_c GREEN "${EPX_BULLET}")
+    else
+      bullet=$(_c RED "${EPX_BULLET}")
+    fi
+
+    names="${bullet} ${names}"
+    visible_names_length=$(printf "%s" "${names}" | sed 's/\x1b\[[0-9;]*m//g' | wc -m)
+    padding=$((names_width - visible_names_length))
+
+    printf "| %-${id_width}s | %s%-${padding}s | %-${image_width}s | %-${status_width}s |\n" "${id}" "${names}" "" "${image}" "${status}"
+  done <<EOF
 ${sorted_data}
 EOF
 
 echo -e "${separator}"
+fi
