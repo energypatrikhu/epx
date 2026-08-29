5410872a15fa08691116894cba1c90be3a401f80
energypatrikhu <energypatrikhu@proton.me>
2026-08-29 23:33:39 +0200

    fix docker compose up command to start running services correctly

--- commands/docker/d.up.sh
+++ commands/docker/d.up.sh
@@ -87,8 +87,10 @@ c_up() {
     docker compose --file "${c_file}" up --pull never --detach --no-build --no-start --yes || true
   fi
 
-  if [[ ${#running_services[@]} -gt 0 && "${opt_stop_exited}" == "false" ]]; then
+  if [[ ${#running_services[@]} -gt 0 && "${opt_stop_exited}" == "true" ]]; then
     docker compose --file "${c_file}" start "${running_services[@]}" || true
+  else
+    docker compose --file "${c_file}" start || true
   fi
 }
 
